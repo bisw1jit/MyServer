c0e11c1ba697d113f99657e6f7d2c82e3db8a16d
LordReaper <39646826+LordReaperY@users.noreply.github.com>
2019-11-09 22:32:03 +0530

    Update nginx.py

--- modules/nginx.py
+++ modules/nginx.py
@@ -1,7 +1,7 @@
 # Tool Name :- MyServer
-# Author :- Rajkumar Dusad
+# Author :- LordReaper
 # Date :- 4/1/2019
-# Powered By :- Aex Software's
+# Powered By :- H1ckPro Software's
 
 import sys
 import os
